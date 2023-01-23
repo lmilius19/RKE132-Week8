de34c1966b4beee3829d35b7026e9157176d5a32
lmilius19 <122237349+lmilius19@users.noreply.github.com>
2023-01-23 03:46:15 +0200

    Add files via upload

---
+++ test 8.1/Program.cs
@@ -0,0 +1,16 @@
+﻿string path = @"C:\Data\";
+string fileName = "chuck.txt";
+
+string[] lines = File.ReadAllLines(Path.Combine(path, fileName));
+
+ShowFileContent(lines);
+
+static void ShowFileContent(string[] fileContentArray)
+{
+    int i = 1;
+    foreach (string line in fileContentArray)
+    {
+        Console.WriteLine($"{i}. {ShowFileContent}");
+        i++;
+    }
+}
